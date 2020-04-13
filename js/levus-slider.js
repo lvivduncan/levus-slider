bde39f48ce1e9310274d6d1ee06598e91b6bbeb4
lvivduncan <lvivduncan@gmail.com>
2020-04-13 23:01:35 +0300

    зміна концепції

--- js/levus-slider.js
+++ js/levus-slider.js
@@ -21,8 +21,18 @@
   // отримуємо ширину екрану
   const windowWidth = document.documentElement.clientWidth;
 
-  // 1 слайд зсуваємо вліво
-  const offset = (1200 - (windowWidth-1200)/2);
-  wrapper.style.transform = `translateX(-${offset}px)`;
+  // отримуємо значення для першого зсуву вліво
+  let offset = (1200 - (windowWidth-1200)/2);
+  
+  // зсуваємо усе вліво при завантаженні
+  items.forEach(item => item.style.transform = `translateX(-${offset}px)`);
 
+  // гортаємо вліво
+  left.addEventListener('click', () => {
+    offset += 1200;
+    items.forEach(item => item.style.transform = `translateX(-${offset}px)`);
+  });
+
+  // гортаємо вправо
+  right.addEventListener();
 }
