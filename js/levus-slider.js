9c85dc91fc2eaad7809e323c118a33839b7d6d91
lvivduncan <lvivduncan@gmail.com>
2020-04-24 20:19:04 +0300

    нова ідея

--- js/levus-slider.js
+++ js/levus-slider.js
@@ -4,7 +4,7 @@
   const $ = document.querySelectorAll.bind(document);
 
   // усі блоки з картинками
-  const items = $('#levus-slider .item');
+  const items = $('#levus-slider > div');
 
   // кнопка вліво
   const left = $('#levus-slider .left')[0];
@@ -12,28 +12,43 @@
   // кнопка вправо
   const right = $('#levus-slider .right')[0];
 
-  // отримуємо ширину екрану
-  const windowWidth = document.documentElement.clientWidth;
+  /* // отримуємо ширину екрану
+  const windowWidth = document.documentElement.clientWidth; */
 
-  // отримуємо значення для першого зсуву вліво
-  let offset = (1200 - (windowWidth - 1200) / 2);
+  // тут будемо зберігати усі розміри
+  const array = [];
 
-  // зсуваємо усе вліво при завантаженні
-  items.forEach(item => item.style.transform = `translateX(-${offset}px)`);
+  // заповнюємо масив розмірами
+  items.forEach((item, i) => {
+    array.push(1200 * (i - 1));
+  });
+
+  console.log(array)
+
+
+  // присвоюємо всім елементам конкретне значення зсуву
+  items.forEach((item, i) => {
+    item.style.transform = `translateX(${array[i]}px)`;
+  });
 
   // гортаємо вліво
   left.addEventListener('click', () => {
-    if (offset < items.length * 1200) { // todo: написати перевірку
-      offset += 1200;
-      items.forEach(item => item.style.transform = `translateX(-${offset}px)`);
-    }
+    const element = array.pop();
+    array.unshift(element);
+
+    items.forEach((item, i) => {
+      item.style.transform = `translateX(${array[i]}px)`;
+    });
   });
 
   // гортаємо вправо
   right.addEventListener('click', () => {
-    if (offset < items.length * 1200) { // todo: написати перевірку
-      offset -= 1200;
-      items.forEach(item => item.style.transform = `translateX(-${offset}px)`);
-    }
+    const element = array.shift();
+    array.push(element);
+
+    items.forEach((item, i) => {
+      item.style.transform = `translateX(${array[i]}px)`;
+    });
   });
+  
 }
