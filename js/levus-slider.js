fbd2c01e027dbf7b9549cbd5d1cfdcf3865642e8
lvivduncan <lvivduncan@gmail.com>
2020-04-28 14:10:27 +0300

    додав фішки

--- js/levus-slider.js
+++ js/levus-slider.js
@@ -7,17 +7,17 @@
   const items = $('#levus-slider > div');
 
   // кнопка вліво
-  const left = $('#levus-slider .left')[0];
+  const left = $('#levus-slider-wrapper .left')[0];
 
   // кнопка вправо
-  const right = $('#levus-slider .right')[0];
+  const right = $('#levus-slider-wrapper .right')[0];
 
   // тут будемо зберігати усі розміри
-  const array = [];
+  const sizes = [];
 
   // заповнюємо масив розмірами
   items.forEach((item, i) => {
-    array.push(1200 * (i - 1));
+    sizes.push(1200 * (i - 1));
   });
 
   // z-index
@@ -25,42 +25,57 @@
 
   // присвоюємо всім елементам конкретне значення зсуву
   items.forEach((item, i) => {
-    item.style.transform = `translateX(${array[i]}px)`;
+    item.style.transform = `translateX(${sizes[i]}px)`;
 
-    if(array[i] === 0) num = 2;
-    if(array[i] > 0 || array[i] < 0) num = 1;
-    if(array[i] >= 1200*2 || array[i] <= -1200*2) num = 0;
+    if(sizes[i] === 0) num = 2;
+    if(sizes[i] > 0 || sizes[i] < 0) num = 1;
+    if(sizes[i] >= 1200*2 || sizes[i] <= -1200*2) num = 0;
     item.style.zIndex = num;
   });
 
   // гортаємо вліво
   left.addEventListener('click', () => {
-    const element = array.pop();
-    array.unshift(element);
+    const element = sizes.pop();
+    sizes.unshift(element);
 
     items.forEach((item, i) => {
-      item.style.transform = `translateX(${array[i]}px)`;
+      item.style.transform = `translateX(${sizes[i]}px)`;
 
-      if(array[i] === 0) num = 2;
-      if(array[i] > 0 || array[i] < 0) num = 1;
-      if(array[i] >= 1200*2 || array[i] <= -1200*2) num = 0;
+      if(sizes[i] === 0) num = 2;
+      if(sizes[i] > 0 || sizes[i] < 0) num = 1;
+      if(sizes[i] >= 1200*2 || sizes[i] <= -1200*2) num = 0;
       item.style.zIndex = num;
     });
   });
 
   // гортаємо вправо
   right.addEventListener('click', () => {
-    const element = array.shift();
-    array.push(element);
+    const element = sizes.shift();
+    sizes.push(element);
 
     items.forEach((item, i) => {
-      item.style.transform = `translateX(${array[i]}px)`;
+      item.style.transform = `translateX(${sizes[i]}px)`;
 
-      if(array[i] === 0) num = 2;
-      if(array[i] > 0 || array[i] < 0) num = 1;
-      if(array[i] >= 1200*2 || array[i] <= -1200*2) num = 0;
+      if(sizes[i] === 0) num = 2;
+      if(sizes[i] > 0 || sizes[i] < 0) num = 1;
+      if(sizes[i] >= 1200*2 || sizes[i] <= -1200*2) num = 0;
       item.style.zIndex = num;
     });
   });
   
+  // autoscroll
+  setInterval( () => {
+    const element = sizes.pop();
+    sizes.unshift(element);
+
+    items.forEach((item, i) => {
+      item.style.transform = `translateX(${sizes[i]}px)`;
+
+      if(sizes[i] === 0) num = 2;
+      if(sizes[i] > 0 || sizes[i] < 0) num = 1;
+      if(sizes[i] >= 1200*2 || sizes[i] <= -1200*2) num = 0;
+      item.style.zIndex = num;
+    });
+  }, 3500);
+
 }
